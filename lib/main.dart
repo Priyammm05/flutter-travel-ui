59a457433e2cc5daae2c5754d4591514770919c1
Priyam <priyam05soni@gmail.com>
2021-10-11 13:12:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/screens/home_screen.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Travel UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        // ignore: deprecated_member_use
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
